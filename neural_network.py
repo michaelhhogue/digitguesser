9f952298706b62fff01cf3dc2c45b60860985717
Michael Hogue <michael.hogue.19@gmail.com>
2023-08-11 19:16:45 -0400

    Trained model

---
+++ neural_network.py
@@ -0,0 +1,19 @@
+from torch import nn
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.model = nn.Sequential(
+            nn.Linear(28*28, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.model(x)
+        return logits
+
--- trainer.py
+++ trainer.py
@@ -2,34 +2,57 @@ import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from torch import nn
+from neural_network import NeuralNetwork
 
 # Hyperparameters
 learning_rate = 0.001
 batch_size = 64
-epochs = 5
+epochs = 10
 
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.model = nn.Sequential(
-            nn.Linear(28*28, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Linear(64, 10)
-        )
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
 
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.model(x)
-        return logits
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            print(f"({((batch + 1) * len(X)):>6d}/{size:<6d}) | Loss: {loss.item()}")
+
+def test_loop(dataloader, model, loss_fn):
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 if __name__ == "__main__":
+    # Define model
+    model = NeuralNetwork()
+
+    load_existing_model = input("Would you like to load an existing model? (Y/n): ")
+    if load_existing_model.upper() == 'Y':
+        model_file_name = input("Enter the model file name: ")
+        model.load_state_dict(torch.load(model_file_name))
+
     print("Loading transform composition...")
 
-    # Convert to tensor and normalize with mean=0.5, std=0.5
-    # for the grayscale pixel intensities
     transform = transforms.Compose(
             [transforms.ToTensor(),
              transforms.Normalize((0.5,), (0.5,))])
@@ -54,5 +77,16 @@ if __name__ == "__main__":
     train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
     test_loader = DataLoader(test_data, batch_size=64, shuffle=True)
 
-    # Define model
-    model = NeuralNetwork()
+    # Loss function and optimizer
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    # Perform training and testing loop epochs
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train_loop(train_loader, model, loss_fn, optimizer)
+        test_loop(test_loader, model, loss_fn)
+    print("Done!")
+
+    save_file_name = input("Save model as (filename): ")
+    torch.save(model.state_dict(), save_file_name)
